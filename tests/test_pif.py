9136a90a201b6ef220368e40c44061f82066971f
Paul Adamson <Paul.adamson@nrl.navy.mil>
2020-07-24 11:01:27 -0400

    make build fail when tests fail (#59)
    * add codecov to TravisCI
    
    * replace CSVDiagnosticOutput a few more times to pass tests
    
    * use numpy to read in output data and compare to reference data using allclose
    
    * comment out codecov for now...need to connect to coveralls.io or similar service or use github actions for coverage

--- tests/test_pif.py
+++ tests/test_pif.py
@@ -3,9 +3,12 @@
 This integration test simply runs the particle_in_field app and compares the output files
 to "good" output.
 """
-import io
+import numpy as np
 
 def test_pif(pif_run):
     for filename in ['e_0.5', 'grid', 'particle_p', 'particle_x', 'time']:
-        assert (list(io.open(f'tests/fixtures/particle_in_field/output/{filename}.csv')) ==
-                list(io.open(f'tmp/particle_in_field/output/{filename}.csv')))
+        ref_data = np.genfromtxt(f'tests/fixtures/particle_in_field/output/{filename}.csv',
+                              delimiter=',')
+        tmp_data = np.genfromtxt(f'tmp/particle_in_field/output/{filename}.csv',
+                              delimiter=',')
+        assert (np.allclose(ref_data, tmp_data, rtol=1e-05, atol=1e-08))
