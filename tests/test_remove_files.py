fc6a5a1becf38be642ac7effe51ff105ebc14766
Kevin Phlips <66272415+kphlips@users.noreply.github.com>
2020-08-25 12:41:17 -0400

    Add docstring

--- tests/test_remove_files.py
+++ tests/test_remove_files.py
@@ -1,8 +1,10 @@
+"""Removes extra directories after testing is completed"""
 from pathlib import Path
 import shutil
 
 
 def test_should_remove_files():
+    """Finds the parent directory (turbopy) and removes the specified files"""
     parent = Path(__file__).parents[1]
     if Path(f"{parent}/tmp").is_dir():
         shutil.rmtree(Path(f"{parent}/tmp").resolve())
