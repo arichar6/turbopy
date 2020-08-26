6a6ad275c2e0ccc055d81b81fdcc597119a60dc6
Kevin Phlips <66272415+kphlips@users.noreply.github.com>
2020-08-26 10:45:00 -0400

    Lint conftest.py

--- conftest.py
+++ conftest.py
@@ -1,9 +1,9 @@
-import pytest
 from pathlib import Path
 import shutil
+import pytest
 
 
-def pytest_unconfigure(config):
+def pytest_unconfigure():
     """Finds the parent directory (turbopy) and removes the specified folders"""
     parent = Path(__file__).parents[0]
     if Path(f"{parent}/tmp").is_dir():
