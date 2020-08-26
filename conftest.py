a25129ffa7342289a4d31bf657a7d3e3f31901be
Kevin Phlips <66272415+kphlips@users.noreply.github.com>
2020-08-26 18:46:38 -0400

    Rearrange code to facilitate additions

--- conftest.py
+++ conftest.py
@@ -4,9 +4,9 @@ import pytest
 
 
 def pytest_unconfigure():
-    """Finds the parent directory (turbopy) and removes the specified folders"""
+    """Locates and removes the specified directories after testing"""
+    directories = ["tmp", "default_output"]
     parent = Path(__file__).parents[0]
-    if Path(f"{parent}/tmp").is_dir():
-        shutil.rmtree(Path(f"{parent}/tmp").resolve())
-    if Path(f"{parent}/default_output").is_dir():
-        shutil.rmtree(Path(f"{parent}/default_output").resolve())
+    for file in directories:
+        if Path(f"{parent}/{file}").is_dir():
+            shutil.rmtree(Path(f"{parent}/{file}").resolve())
