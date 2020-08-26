782cc25a3995ed4264696dfd985dfcf9862df14a
Kevin Phlips <66272415+kphlips@users.noreply.github.com>
2020-08-26 18:49:01 -0400

    Rename variables

--- conftest.py
+++ conftest.py
@@ -4,9 +4,9 @@ import pytest
 
 
 def pytest_unconfigure():
-    """Locates and removes the specified directories after testing"""
-    directories = ["tmp", "default_output"]
+    """Locates and removes the specified files after testing"""
+    files = ["tmp", "default_output"]
     parent = Path(__file__).parents[0]
-    for file in directories:
+    for file in files:
         if Path(f"{parent}/{file}").is_dir():
             shutil.rmtree(Path(f"{parent}/{file}").resolve())
