f33e9baed1dfd6d3d45eedb88cab25e279511933
Kevin Phlips <66272415+kphlips@users.noreply.github.com>
2020-08-26 10:42:48 -0400

    Move method to conftest.py

--- conftest.py
+++ conftest.py
@@ -1,1 +1,12 @@
 import pytest
+from pathlib import Path
+import shutil
+
+
+def pytest_unconfigure(config):
+    """Finds the parent directory (turbopy) and removes the specified folders"""
+    parent = Path(__file__).parents[0]
+    if Path(f"{parent}/tmp").is_dir():
+        shutil.rmtree(Path(f"{parent}/tmp").resolve())
+    if Path(f"{parent}/default_output").is_dir():
+        shutil.rmtree(Path(f"{parent}/default_output").resolve())
