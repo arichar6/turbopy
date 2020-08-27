158be6ffbc87e4c7def50e8c384f3b0a25e23834
Kevin Phlips <66272415+kphlips@users.noreply.github.com>
2020-08-27 18:19:58 -0400

    Implement tmp_path for p.i.f.

--- tests/test_pif.py
+++ tests/test_pif.py
@@ -4,12 +4,16 @@ This integration test simply runs the particle_in_field app and compares the out
 to "good" output.
 """
 import numpy as np
-
+import shutil
 
 def test_pif(pif_run):
     for filename in ['e_0.5', 'grid', 'particle_p', 'particle_x', 'time']:
         ref_data = np.genfromtxt(f'tests/fixtures/particle_in_field/output/{filename}.csv',
                                  delimiter=',')
-        tmp_data = np.genfromtxt(f'tmp/particle_in_field/output/{filename}.csv',
+        tmp_data = np.genfromtxt(f'tmp_path/particle_in_field/output/{filename}.csv',
                                  delimiter=',')
         assert (np.allclose(ref_data, tmp_data, rtol=1e-05, atol=1e-08))
+
+
+def test_should_remove_tmp_path_directory():
+    shutil.rmtree("tmp_path")
