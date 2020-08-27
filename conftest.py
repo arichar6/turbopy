1030a21f2d5a47e960be610a1f4c7bc65cc5223f
Kevin Phlips <66272415+kphlips@users.noreply.github.com>
2020-08-27 18:10:07 -0400

    Remove old conftest.py file deletion method

--- conftest.py
+++ conftest.py
@@ -1,12 +1,1 @@
-from pathlib import Path
-import shutil
 import pytest
-
-
-def pytest_unconfigure():
-    """Locates and removes the specified files after testing"""
-    files = ["tmp", "default_output"]
-    parent = Path(__file__).parents[0]
-    for file in files:
-        if Path(f"{parent}/{file}").is_dir():
-            shutil.rmtree(Path(f"{parent}/{file}").resolve())
