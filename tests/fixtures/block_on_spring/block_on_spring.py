261c9860f21767a3ff768e0eac0940c1d5147b52
Grace Tang <49345480+gracetangg@users.noreply.github.com>
2020-11-05 07:50:54 -0500

    add gridless simulation capabilities  (#164)
    * add gridless capability
    
    * remove grid from block_on_spring
    
    * adds warning
    
    * use python warnings

--- tests/fixtures/block_on_spring/block_on_spring.py
+++ tests/fixtures/block_on_spring/block_on_spring.py
@@ -122,7 +122,6 @@ ComputeTool.register("Leapfrog", Leapfrog)
 def bos_run():
     # Note: grid isn't used, but "gridless" sims aren't an option yet
     problem_config = {
-        "Grid": {"N": 2, "x_min": 0, "x_max": 1},
         "Clock": {"start_time": 0,
                   "end_time": 10,
                   "num_steps": 10},
