b2382ae5164063cf69dca46e93f208196f3106fc
Nancy <nisner@syntek.org>
2023-06-08 22:33:30 -0500

    sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -10,6 +10,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 
+import sphinx_rtd_theme
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
@@ -68,8 +69,9 @@ master_doc = 'index'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
-html_static_path = ['_static']
+#html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
+html_static_path = []
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
